2a31bab59eb4811a47cf9bd44424f2bfd9c0d0dd
SidaRevamped <sida@tutanota.com>
2016-06-22 12:38:37 -0400

    Revert "Revert "Add files via upload""
    This reverts commit 323ea7db4df097d872b2c21af5d4503d107c5cd6.

---
+++ Oktw/Loader/Loader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loader
+{
+    class Loader
+    {
+        static void Main(string[] args)
+        {
+            EloBuddy.SDK.Events.Loading.OnLoadingComplete += Initialize;
+        }
+        static void Initialize(EventArgs args)
+        {
+            LeagueSharp.SDK.Bootstrap.Init();
+            if (EloBuddy.Player.Instance.ChampionName == "Ryze")
+            {
+                Ryze.RyzeLoading();
+            }
+        }
+    }
+}
